a86b88eb45aaa0f5a4c9c1033aff6ec17aae94ec
oxlotel21 <94632508+oxlotel21@users.noreply.github.com>
2022-01-24 17:24:03 +0100

    fix override

--- app/src/main/java/de/freifunk/powa/MainActivity.kt
+++ app/src/main/java/de/freifunk/powa/MainActivity.kt
@@ -67,25 +67,4 @@ class MainActivity : AppCompatActivity() {
 
         requestAllPermissions(this@MainActivity)
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == GeneralPermissionRequestCode) {
-            for (resultIndex in grantResults.indices) {
-                if (grantResults[resultIndex] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this@MainActivity,
-                        "Permission granted: ${PERMISSIONS[resultIndex]}",
-                        Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this@MainActivity,
-                        "Permission denied: ${PERMISSIONS[resultIndex]}",
-                        Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
 }
