2f99c1fc88685a66c572ce75fb422d7e12703f16
PumarPap <60217073+PumarPap@users.noreply.github.com>
2022-01-25 19:02:52 +0100

    change second function

--- app/src/main/java/de/freifunk/powa/storeIntern/StoreImageUtilities.kt
+++ app/src/main/java/de/freifunk/powa/storeIntern/StoreImageUtilities.kt
@@ -1,16 +1,20 @@
-package de.freifunk.powa.store_intern
+package de.freifunk.powa.storeIntern
 
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.io.IOException
 import java.lang.Exception
+import java.nio.file.Paths
+
+const val mapDir = "maps"
 
 fun saveBitmapToInternalStorage(context: Context, filename: String, bitmap: Bitmap): Boolean {
     return try {
-        context.openFileOutput("$filename.jpg", Context.MODE_PRIVATE).use {
+        context.openFileOutput("$mapDir${File.separator}$filename.jpg", Context.MODE_PRIVATE).use {
             if(!bitmap.compress(Bitmap.CompressFormat.JPEG, 95, it))
                 throw IOException("Bitmap of file \"$filename\", couldn't be saved!")
         }
@@ -21,21 +25,21 @@ fun saveBitmapToInternalStorage(context: Context, filename: String, bitmap: Bitm
     }
 }
 
-suspend fun loadListOfInternalStorageImages(context: Context): List<InternalStorageImage> {
-    return withContext(Dispatchers.IO) {
-        context.filesDir.listFiles()?.filter {
-            it.canRead() && it.isFile && it.name.endsWith(".jpg")
-        }?.map {
-            val bytes = it.readBytes()
-            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            InternalStorageImage(it.name, bitmap)
-        } ?: listOf()
-    }
+fun loadListOfInternalStorageImages(context: Context): List<InternalStorageImage> {
+    val fD = context.filesDir
+    val maps = Paths.get(fD.path+File.separator+ mapDir).toFile()
+    return  maps.listFiles()?.filter {
+        it.canRead() && it.isFile && it.name.endsWith(".jpg")
+    }?.map {
+        val bytes = it.readBytes()
+        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        InternalStorageImage(it.name, bitmap)
+    } ?: listOf()
 }
 
 fun deleteFileFromInternalStorage(context: Context, filename: String): Boolean {
     return try {
-        context.deleteFile(filename)
+        context.deleteFile("$mapDir${File.separator}$filename.jpg")
     } catch (e: Exception) {
         e.printStackTrace()
         false
