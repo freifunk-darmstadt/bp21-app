a837c353ce99ea9e38c8ab1e5581f158592e32b2
PumarPap <60217073+PumarPap@users.noreply.github.com>
2022-02-28 18:04:12 +0100

    add functions for parsing maps to a JSON-File

--- app/src/main/java/de/freifunk/powa/api/ExportConsumerJSON.kt
+++ app/src/main/java/de/freifunk/powa/api/ExportConsumerJSON.kt
@@ -1,6 +1,7 @@
 package de.freifunk.powa.api
 
 import de.freifunk.powa.model.Map
+import de.freifunk.powa.model.ScanInformation
 import de.freifunk.powa.model.WiFiScanObject
 import java.io.File
 
@@ -10,41 +11,56 @@ class ExportConsumerJSON : ExportConsumer("JSON Exporter", "no description") {
     override fun export(file: File, maps: List<Map>) {
         if(!file.exists())
             return
+        file.writeText(mapToString(maps[0])) //TODO: export to JSON
 
     }
 
-    private fun exportMap(map: Map): String {
-        //TODO(finish exportMap())
-       return """
-            {
-                "Name" : "${map.name}",
-                "Location" : "${map.location}",
-                "Image" : "Bitmap",
-                "Scans" : [ScanObject.JSON]
-            }
-        """.trimIndent()
+    private fun mapToString(map: Map): String{
+        return "{" +
+                "\"name\": \"${map.name}\"," +
+                "\"location\": \"${map.name}\"," +
+                "\"scans:\": ${listOfScansToString(map.scans)}" +
+                "}"
     }
 
-    private fun exportWifiScanObject(wso: WiFiScanObject): String{
-        //TODO(finish exportWifiScanObject())
-        return """
-            {
-                "BSSID" : "${wso.bssid}",
-                "Capabilities" : "${wso.capabilities}",
-                "Center Freq 0" : ${wso.centerFreq0},
-                "Center Freq 1" : ${wso.centerFreq1},
-                "Channel Width" : ${wso.channelWidth},
-                "Frequency" : ${wso.frequency},
-                "Information ID" : ${wso.informationID},
-                "Level" : ${wso.level},
-                "Operator Friendly Name" : ${wso.operatorFriendlyName},
-                "Scan Information" : "ScanInformation?",
-                "SSID" : "${wso.ssid}",
-                "Timestamp" : "${wso.timestamp}",
-                "Venue Name" : "${wso.timestamp}",
-                "X Coordinate" : ${wso.xCoordinate},
-                "Y Coordinate" : ${wso.yCoordinate}
-            }
-        """.trimIndent()
+    private fun listOfScansToString(scans: List<WiFiScanObject>): String {
+        var retString = "{"
+        for (wso in scans){
+            retString = "$retString${wiFiScanObjectToString(wso)},"
+        }
+        retString.removeSuffix(",")
+        retString = "$retString}"
+        return retString
+    }
+
+    private fun wiFiScanObjectToString(wso: WiFiScanObject): String {
+        return "{"+
+                "\"bssid\": \"${wso.bssid}\"," +
+                "\"ssid\": \"${wso.ssid}\"," +
+                "\"capabilities\": \"${wso.capabilities}\"," +
+                "\"centerFreq0\": ${wso.centerFreq0}," +
+                "\"centerFreq1\": ${wso.centerFreq1}," +
+                "\"channelWidth\": ${wso.channelWidth}," +
+                "\"frequency\": ${wso.frequency}," +
+                "\"level\": ${wso.level}," +
+                "\"operatorFriendlyName\": \"${wso.operatorFriendlyName}\"," +
+                "\"venueName\": ${wso.venueName}," +
+                "\"xCoordinate\": ${wso.xCoordinate}," +
+                "\"yCoordinate\": ${wso.yCoordinate}," +
+                "\"informationID\": ${wso.informationID}," +
+                "\"scanInformation\": ${scanInformationToString(wso.scanInformation)}," +
+                "\"timestamp\": \"${wso.timestamp}\"" +
+                "}"
+    }
+
+    private fun scanInformationToString(si: ScanInformation?): String {
+        if (si == null)
+            return "null"
+
+        val data = "ByteArray" //TODO: transform a ByteArray to a String
+        return "{" +
+                "\"id\": ${si.id}," +
+                "\"data\": $data" +
+        "}"
     }
 }
